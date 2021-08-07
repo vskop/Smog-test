13e7c75557064f1dd44bd4ed00ee4ed54d8e9ea9
Vasyl Skop <vskop@concisesoftware.com>
2021-08-07 09:52:55 +0200

    Initial Commit

---
+++ Smog test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smog test
+//
+//  Created by Vasyl Skop on 07/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
